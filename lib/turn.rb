590086037268b5d4a5a2bc87d9266b958882cc3b
Ryan Schmelter <rschmelter33@gmail.com>
2017-09-26 01:24:59 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,45 @@
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def position_taken?(board, index)
+  board[index] != " " && board[index] != "" && board[index] != nil
+end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, input, token = "X")
+  board[input] = token
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if !valid_move?(board, index)
+    turn(board)
+  else
+    move(board, index, token = "X")
+  end
+end
+
+  # while index != valid_move?(board, index)
+  #   puts "Please make a valid move"
+  #   gets.strip
+  #   input = gets.strip
+  #   index = input_to_index(input)
+  # end
+
+  #
+  # end
